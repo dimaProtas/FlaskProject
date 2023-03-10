2e408458f538698b19fb75e12f59e0e44298fdb7
dima protasevich <dima_protasevich92@mail.ru>
2023-03-10 18:41:19 +0300

    create models Tag, article_tag and conection

--- blog/models/article.py
+++ blog/models/article.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from sqlalchemy import Column, Integer, ForeignKey, String, Text, DateTime, func
 from sqlalchemy.orm import relationship
+
+from blog.models.article_tag import article_tag_association_table
 from blog.models.database import db
 
 
@@ -14,3 +16,4 @@ class Article(db.Model):
     dt_updated = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     author = relationship("Author", back_populates="articles")
+    tags = relationship("Tag", back_populates="articles", secondary=article_tag_association_table)
---
+++ blog/models/tag.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from blog.models.article_tag import article_tag_association_table
+from blog.models.database import db
+
+
+class Tag(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150), nullable=False, default="", server_default="")
+
+    article = relationship("Article", back_populates="tags", secondary=article_tag_association_table)
